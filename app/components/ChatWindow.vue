13471c95bf94d73888d4b16441a905b38344f484
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2025-07-30 14:57:01 +0800

    feat: markdown rendering with Nuxt MDC

--- app/components/ChatWindow.vue
+++ app/components/ChatWindow.vue
@@ -47,7 +47,7 @@ watch(() => messages, pinToBottom, { deep: true });
             }"
           >
             <div class="message-content">
-              {{ message.content }}
+              <MarkdownRenderer :content="message.content" />
             </div>
           </div>
 
