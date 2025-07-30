99434683ca04ce67db677923175018d1f7db7490
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2025-07-30 14:57:01 +0800

    feat: markdown rendering with Nuxt MDC

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/test-utils/module'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/test-utils/module', '@nuxtjs/mdc'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     openaiApiKey: '',
@@ -13,6 +13,12 @@ export default defineNuxtConfig({
   vite: {
     optimizeDeps: {
       include: ['debug'],
+    },
+  },
+  mdc: {
+    highlight: {
+      theme: 'material-theme-palenight',
+      langs: ['html', 'markdown', 'vue', 'typescript', 'javascript'],
     },
   },
 });
